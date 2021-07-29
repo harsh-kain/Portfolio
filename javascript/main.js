02c18d2fac6ad060ca601d290b35d9f64aac8632
harshkain415 <harshkain1899@gmail.com>
2021-07-29 15:35:30 +0530

    Second commit

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,7 @@ resize = () =>{
     let afterNav = document.getElementById("after-nav")
 
 
-   if(inner_width < 780){
+   if(inner_width < 700){
        navbar.classList.add("navbar-remove")
        afterNav.classList.add("add-after-nav")
 
@@ -74,7 +74,11 @@ education.addEventListener('click' , ()=>{
 
 let flag = 0;
 
+showSlide = document.getElementsByClassName('slides');
 
+// slideShower = () => {
+//     showSlide.style.display = 'none';
+// }
 
 slideshow_controller = (x) => {
     flag = flag + x;
@@ -86,20 +90,22 @@ slideshow = (num) => {
         flag = 0;
         num = 0
     }
-
+    
     if (num < 0 ){
         flag = slide.length - 1;
         num = slide.length - 1;
     }
     for(let y of slide){
         y.style.display = 'none'
-
+        
     }
-
+    
     slide[num].style.display = 'block'
     slide[num].style.display = 'flex'
     
 } 
+
+slideshow_controller(0);
 
 // Dark theme start here 
 
@@ -107,13 +113,46 @@ const moon = document.getElementById("night-mode");
 const darkNavbar = document.getElementById('navbar')
 const aTag = document.getElementsByTagName('a');
 const sunIcon = document.getElementById('sun')
+const afterNavDarkMode = document.getElementById("night-mode-after");
+
+const darkNavbarAfter = document.getElementById('after-nav');
+
+let day = document.getElementById('day-mode');
+let night = document.getElementById('night-mode');
 
 
 
+afterNavDarkMode.addEventListener('click' , () =>{
+    document.body.classList.toggle('dark-theme');
+    darkNavbarAfter.classList.toggle('dark-navbar')
+})
+
+
+
+
+showDay = () => {
+    day.style.display = 'block';
+    night.style.display = 'none';
+}
+
+showNight = () => {
+    night.style.display = 'none';
+    day.style.display = 'block';
+}
+
+
+day.addEventListener('click' , ()=>{
+    document.body.classList.toggle('dark-theme');
+    darkNavbar.classList.toggle('dark-navbar');
+    showDay();
+
+})
 moon.addEventListener('click' , () => {
     document.body.classList.toggle('dark-theme');
     darkNavbar.classList.toggle('dark-navbar');
     aTag.classList.toggle('dark-navbar a');
+    showNight();
+    
 
 })
 
